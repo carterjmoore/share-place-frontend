ee42bfb308555a6736d816f346fc9108a01402c0
Carter Moore <carterjmoore02@gmail.com>
2022-10-13 20:16:34 -0300

    send image to backend

--- src/user/pages/Auth.jsx
+++ src/user/pages/Auth.jsx
@@ -67,8 +67,6 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
-    console.log(formState.inputs);
-
     if (isLoginMode) {
       try {
         const responseData = await sendRequest(
@@ -85,15 +83,16 @@ const Auth = () => {
       } catch (err) {}
     } else {
       try {
+        const formData = new FormData();
+        formData.append('email', formState.inputs.email.value);
+        formData.append('name', formState.inputs.name.value);
+        formData.append('password', formState.inputs.password.value);
+        formData.append('image', formState.inputs.image.value);
+
         const responseData = await sendRequest(
           '/users/signup',
           'POST',
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-          { 'Content-Type': 'application/json' }
+          formData
         );
 
         auth.login(responseData.user.id);
